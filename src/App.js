b1978f35f71603bd2cf5864c2157c87d779a3b89
Felizardo <devfelizardo@gmail.com>
2020-11-02 02:58:16 -0300

    importing images and css inside js file with reactJS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import bkgImage from "./assets/bkg.jpg";
 
 function App() {
   const [projects, setProjects] = React.useState(["Projeto 1", "Projeto 2"]);
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Header title="Contacts" />
+      <img width={300} src={bkgImage} />
       <ul>
         {projects.map((elem) => (
           <li key={elem}>{elem}</li>
